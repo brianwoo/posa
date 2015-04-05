1a6e726d18e958ed815c55d6ebc5308bb0d2ef3e
Brian Woo <bwoo@bwooAndroid.(none)>
2015-04-04 20:41:18 -0600

    Reworked POSA directories

---
+++ POSA-14-master/assignments/week-2-assignment-1/src/edu/vuum/mocca/SimpleAtomicLong.java
@@ -0,0 +1,128 @@
+// Import the necessary Java synchronization and scheduling classes.
+
+package edu.vuum.mocca;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @class SimpleAtomicLong
+ *
+ * @brief This class implements a subset of the
+ *        java.util.concurrent.atomic.SimpleAtomicLong class using a
+ *        ReentrantReadWriteLock to illustrate how they work.
+ */
+class SimpleAtomicLong
+{
+    /**
+     * The value that's manipulated atomically via the methods.
+     */
+    private long mValue;
+
+
+    /**
+     * The ReentrantReadWriteLock used to serialize access to mValue.
+     */
+    // TODO - add the implementation
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    /**
+     * Creates a new SimpleAtomicLong with the given initial value.
+     */
+    public SimpleAtomicLong(long initialValue) {
+        // TODO - you fill in here
+    	this.mValue = initialValue;
+    }
+
+    /**
+     * @brief Gets the current value
+     * 
+     * @returns The current value
+     */
+    public long get() {
+        // TODO - you fill in here
+    	try
+    	{
+    		readWriteLock.readLock().lock();
+    		return mValue;
+    	}
+    	finally
+    	{
+    		readWriteLock.readLock().unlock();	
+    	}
+    }
+
+    /**
+     * @brief Atomically decrements by one the current value
+     *
+     * @returns the updated value
+     */
+    public long decrementAndGet() {
+        // TODO - you fill in here
+    	try
+    	{
+    		readWriteLock.writeLock().lock();
+    		return --mValue;
+    	}
+    	finally
+    	{
+    		readWriteLock.writeLock().unlock();	
+    	}
+    }
+
+    /**
+     * @brief Atomically increments by one the current value
+     *
+     * @returns the previous value
+     */
+    public long getAndIncrement() {
+        // TODO - you fill in here
+    	try
+    	{
+    		readWriteLock.writeLock().lock();
+    		return mValue++;
+    	}
+    	finally
+    	{
+    		readWriteLock.writeLock().unlock();	
+    	}
+    }
+
+    /**
+     * @brief Atomically decrements by one the current value
+     *
+     * @returns the previous value
+     */
+    public long getAndDecrement() {
+        // TODO - you fill in here
+    	try
+    	{
+    		readWriteLock.writeLock().lock();
+    		return mValue--;
+    	}
+    	finally
+    	{
+    		readWriteLock.writeLock().unlock();	
+    	}
+    }
+
+    /**
+     * @brief Atomically increments by one the current value
+     *
+     * @returns the updated value
+     */
+    public long incrementAndGet() {
+        // TODO - you fill in here
+    	try
+    	{
+    		readWriteLock.writeLock().lock();
+    		return ++mValue;
+    	}
+    	finally
+    	{
+    		readWriteLock.writeLock().unlock();	
+    	}
+    }
+}
+
---
+++ POSA-14-master/assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -0,0 +1,130 @@
+package edu.vuum.mocca;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
+
+/**
+ * @class SimpleSemaphore
+ * 
+ * @brief This class provides a simple counting semaphore
+ *        implementation using Java a ReentrantLock and a
+ *        ConditionObject (which is accessed via a Condition). It must
+ *        implement both "Fair" and "NonFair" semaphore semantics,
+ *        just liked Java Semaphores.
+ */
+public class SimpleSemaphore {
+    /**
+     * Define a Lock to protect the critical section.
+     */
+    // TODO - you fill in here
+	private Lock mLock;
+
+    /**
+     * Define a Condition that waits while the number of permits is 0.
+     */
+    // TODO - you fill in here
+	private Condition mPermitIsZero; 
+	
+
+    /**
+     * Define a count of the number of available permits.
+     */
+    // TODO - you fill in here.  Make sure that this data member will
+    // ensure its values aren't cached by multiple Threads..
+	private  int mNumOfPermitsAvail;
+	
+
+    public SimpleSemaphore(int permits, boolean fair) {
+        // TODO - you fill in here to initialize the SimpleSemaphore,
+        // making sure to allow both fair and non-fair Semaphore
+   		mLock = new ReentrantLock(fair);    	
+   		mPermitIsZero = mLock.newCondition();
+    	
+    	mNumOfPermitsAvail = permits;
+    }
+
+    /**
+     * Acquire one permit from the semaphore in a manner that can be
+     * interrupted.
+     */
+    public void acquire() throws InterruptedException {
+        // TODO - you fill in here.
+    	
+    	try
+    	{
+        	mLock.lockInterruptibly();
+        	
+    		while (mNumOfPermitsAvail == 0)
+    			mPermitIsZero.await();  // await() is interruptible
+    	
+    		mNumOfPermitsAvail--;
+    	}
+    	finally
+    	{
+    		mLock.unlock();
+    	}
+    }
+
+    /**
+     * Acquire one permit from the semaphore in a manner that cannot be
+     * interrupted.
+     */
+    public void acquireUninterruptibly() {
+        // TODO - you fill in here.
+    	
+    	try
+		{
+			mLock.lock();
+			
+	    	while (mNumOfPermitsAvail == 0)
+	    		mPermitIsZero.awaitUninterruptibly();
+	    	
+	    	mNumOfPermitsAvail--;
+		}
+    	finally
+    	{
+    		mLock.unlock();
+    	}
+    	
+    }
+
+    /**
+     * Return one permit to the semaphore.
+     */
+    public void release() {
+        // TODO - you fill in here.
+    	
+    	try
+    	{
+    		mLock.lock();
+    	
+    		mNumOfPermitsAvail++;
+    	
+    		mPermitIsZero.signal();
+    	}
+    	finally
+    	{
+    		mLock.unlock();
+    	}
+
+    }
+
+    /**
+     * Return the number of permits available.
+     */
+    public int availablePermits() {
+        // TODO - you fill in here by changing null to the appropriate
+        // return value.
+    	try
+    	{
+    		mLock.lock();
+    	
+    		return mNumOfPermitsAvail;
+    	}
+    	finally
+    	{
+    		mLock.unlock();
+    	}
+    }
+}
